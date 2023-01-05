504afdaedcf20271abbc25c1ab999dd0d6a08212
Erick Douglas <108193587+Haziir@users.noreply.github.com>
2023-01-05 20:54:58 -0300

    first commit

---
+++ quiz.js
@@ -0,0 +1,18 @@
+<div class="imagens-pc">
+    <img src="APPDEPERGUNTAS/imagens/pc/1.jpg" alt="imagem do canada" width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/2.jpg" alt="imagem do canada" width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/3.jpg" alt="imagem do canada" width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/4.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/5.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/6.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/7.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/8.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/9.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/10.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/11.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/12.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/13.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/14.jpg" alt="imagem do canada"width="400" height="400">
+    <img src="APPDEPERGUNTAS/imagens/pc/15.jpg" alt="imagem do canada"width="400" height="400">
+</div>
+        
